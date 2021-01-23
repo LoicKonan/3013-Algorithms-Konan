f26a3886afe73cbfce3b577fab736e4d6fedffba
Loic <konanloic10@gmail.com>
2021-01-23 01:30:44 -0600

    README.md

--- Assignments/P01/main.cpp
+++ Assignments/P01/main.cpp
@@ -17,36 +17,6 @@
 *           when program completed.
 * 
 *    Usage:
-*        - $ ./ main.cpp
-*
-*    Files:
-*             main.cpp    : driver program
-******************************************************************************/
-
-#include <iostream>
-#include <fstream>
-
-using namespace std;
-
-/*****************************************************************************
-*
-*    Author:           Loic Konan
-*    Email:            loickonan.lk@gmail.com
-*    Label:            P01
-*    Title:            Assignment 4 - Resizing the Stack
-*    Course:           CMPS 3013
-*    Semester:         Spring 2021
-*    Description:
-*        
-*           This program will read numbers from the file nums.dat. 
-*           If a number is even it will push it onto the stack. If a number
-*           is odd, it will perform a pop operation removing the last 
-*           even value from the stack. The stack cannot get smaller than 10 
-*           (our starting stack size). We need to keep track of the max size
-*           and how many times the stack was resized and final size of stack
-*           when program completed.
-* 
-*    Usage:
 *        Enter main file name, then the input file and the output file.
 *        - $ ./ main.cpp
 *        - $ ./ nums_dat
